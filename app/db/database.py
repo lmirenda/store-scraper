aaa5b80288756b0c5aaa3ead3bf352de6e7522e4
Luca Mirenda <lucamateomirenda@gmail.com>
2024-02-23 17:21:08 -0300

    Setup alembic and postgres db

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import create_engine, Session, SQLModel
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+from migrations.env import DATABASE_URL
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args=connect_args, echo=True)
+engine = create_engine(DATABASE_URL, connect_args=connect_args, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
