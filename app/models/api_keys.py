c40c3614b81f1c823958baea03ef1f743122ac00
Luca Mirenda <lucamateomirenda@gmail.com>
2024-02-23 17:21:19 -0300

    Update apikey model

--- app/models/api_keys.py
+++ app/models/api_keys.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from sqlmodel import SQLModel, Field
 
@@ -7,3 +8,4 @@ class ApiKeyModel(SQLModel, table=True):
     __tablename__ = "api_keys"
     api_key: uuid.UUID = Field(primary_key=True, index=True, default_factory=uuid.uuid4)
     active: bool = Field(default=True)
+    created_at: datetime = Field(default_factory=datetime.now)
